8c3f1cee6a5b2fab0835bb8887c9ee957f139f2d
leedo <leedoyun98@naver.com>
2021-02-22 19:22:35 +0900

    table

--- src/main/java/com/example/demo/rpl/domain/Reply.java
+++ src/main/java/com/example/demo/rpl/domain/Reply.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 import com.example.demo.brd.domain.Board;
 import com.example.demo.usr.domain.User;
 
-@Entity @Table(name="replier")
+@Entity @Table(name="replies")
 public class Reply {
 	@Id @Column(name="rpl_no") private int rplNo;
 	@Column(name="rpl_content") private String rplContent;
